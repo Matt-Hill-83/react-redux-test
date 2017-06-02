ae141bb05a62c323d8591fa829d59a5199fddd6d
KletusVandamme <matt.hill.email@gmail.com>
2017-06-02 13:37:00 -0700

    works

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -6,6 +6,31 @@ import Table from '../containers/Table/Table';
 require('../../scss/style.scss');
 // require('../../scss/Table.css');
 
+
+
+ function handleClick(buttonName) {
+
+  console.log('|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|');
+  console.log('buttonName: ');
+  console.log(buttonName);
+  console.log('|------------------------------------------------------------------------------------------------|')
+  
+  
+  // this.setState(calculate(this.state, buttonName));
+}
+
+
+
+let cols = [
+    { id: 'firstName', label: 'First Name' },
+    { id: 'lastName', label: 'Last Name' }
+];
+
+let data = [
+    { id: 1, firstName: 'John', lastName: 'Doe' },
+    { id: 2, firstName: 'Bob', lastName: 'Marley' }
+];
+
 const App = () => (
     <div>
         <h2>User List</h2>
@@ -13,6 +38,7 @@ const App = () => (
         <hr />
         <h2>User Details</h2>
         <UserDetails />
+    <Table cols={cols} data={data} name="table-1" xxx-clickHandler={handleClick} />    
     </div>
 );
 
