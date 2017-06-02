eef14fdd7f14a90c6f3722853a11a9daa0940b75
KletusVandamme <matt.hill.email@gmail.com>
2017-06-02 14:33:07 -0700

    works

--- dev/js/reducers/reducer-cells.js
+++ dev/js/reducers/reducer-cells.js
@@ -4,30 +4,20 @@
  * */
 
 export default function () {
-    return [
-        {
-            id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
-        },
-        {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
-        },
-        {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
-        }
-    ]
+    let cols = [
+        { id: 'firstName', label: 'First Name' },
+        { id: 'lastName', label: 'Last Name' }
+    ];
+
+    let data = [
+        { id: 1, firstName: 'John', lastName: 'Doe' },
+        { id: 2, firstName: 'Bob', lastName: 'Marley' }
+    ];
+
+    let tableData = {
+        cols: cols,
+        data: data,
+    };
+
+    return tableData;
 }
