ea498ef21350f32fea149ac27205dd64bebf9a98
KletusVandamme <matt.hill.email@gmail.com>
2017-06-02 13:53:34 -0700

    works

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -6,37 +6,45 @@ import Table from '../containers/Table/Table';
 require('../../scss/style.scss');
 // require('../../scss/Table.css');
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total    : null,
+      next     : null,
+      operation: null,
+    };
+  }
 
+	handleClick(buttonName) {
+		console.log('|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|');
+		console.log('buttonName: ');
+		console.log(buttonName);
+		console.log('|------------------------------------------------------------------------------------------------|')
+	}
 
- function handleClick(buttonName) {
+  render() {
+    let cols = [
+        { id: 'firstName', label: 'First Name' },
+        { id: 'lastName', label: 'Last Name' }
+    ];
 
-  console.log('|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|');
-  console.log('buttonName: ');
-  console.log(buttonName);
-  console.log('|------------------------------------------------------------------------------------------------|')
+    let data = [
+        { id: 1, firstName: 'John', lastName: 'Doe' },
+        { id: 2, firstName: 'Bob', lastName: 'Marley' }
+    ];
+
+    return (
+	    <div>
+		    <Table cols={cols} data={data} name="table-1" clickHandler={this.handleClick} />    
+	      <h2>User List</h2>
+	      <UserList />
+	      <hr />
+	      <h2>User Details</h2>
+	      <UserDetails />
+	    </div>
+
+    );
+  }
 }
-
-
-
-let cols = [
-    { id: 'firstName', label: 'First Name' },
-    { id: 'lastName', label: 'Last Name' }
-];
-
-let data = [
-    { id: 1, firstName: 'John', lastName: 'Doe' },
-    { id: 2, firstName: 'Bob', lastName: 'Marley' }
-];
-
-const App = () => (
-    <div>
-	    <Table cols={cols} data={data} name="table-1" clickHandler={handleClick} />    
-      <h2>User List</h2>
-      <UserList />
-      <hr />
-      <h2>User Details</h2>
-      <UserDetails />
-    </div>
-);
-
 export default App;
